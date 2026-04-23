2f32ce7b104601133f76acd58fcfa1482b7741bc
Dina Fridman <dina624048Qgmail.com>
2026-04-24 01:12:56 +0300

    Fixing Dockerfile and paths

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c340da12e668a6053fea0032f876becbf36937bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded481f48385fe3958980ed88c5b7195243ad6b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
