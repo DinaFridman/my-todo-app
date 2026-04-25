f0c7b25a3a9f3568fa0a247164d20f72799b748d
Dina Fridman <dina624048Qgmail.com>
2026-04-25 23:13:17 +0300

    Flatten structure - removing Root folder

--- Root/TodoApi/ToDoDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class ToDoDbContext : DbContext
-{
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options) { }
-    public DbSet<Item> Items { get; set; }
-}
